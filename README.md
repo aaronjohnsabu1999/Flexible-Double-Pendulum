a251039ca94f6082d95e26ea59e52ecbbd5418fb
Aaron John Sabu <38135463+aaronjohnsabu1999@users.noreply.github.com>
2020-04-17 15:38:17 +0530

    Version 03.001

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-## Flexible_Double_Pendulum
+# Flexible_Double_Pendulum
 
 This repository contains files for simulating a flexible double pendulum on Matlab from an assignment in ME6102 (Design of Mechatronic Systems)
 
 ***
+<table>
+  <tr>
+    <td>Working of the Model</td>
+    <td>Initial Condition</td>
+  </tr>
+  <tr>
+    <td><img src="Snapshots_and_Videos/WorkingModel.gif" width=405 height=480></td>
+    <td><img src="Snapshots_and_Videos/InitialCondition.jpg" width=405 height=480></td>
+  </tr>
+</table>
 
+***
 The assignment utilized the _ode.m_ function for solving the ordinary differential equation which was further called in _animation.m_. The equations pertaining to the system are given below:
 ![Equations](https://quicklatex.com/cache3/03/ql_9bca3d4865084e587ea97ba4749c4803_l3.png)
 
 ***
-
 Credits for ODE development: [Nambilakshmi](https://www.github.com/rnambilakshmi)
--- animation.m
+++ animation.m
@@ -3,15 +3,15 @@
 clc;
 clear all;
 
-l0  = 0.5;
+l0  = 0.6;
 l1  = 0.3;
-m0  = 0.02;
-m1  = 0.03;
-k   = 1;
+m0  = 0.03;
+m1  = 0.05;
+k   = 1.5;
 g   = 9.81;
-e00 = (-90.0+40.0)*pi/180.0;
-e10 = (-90.0+10.0)*pi/180.0;
-time  = linspace(0,100,50000);
+e00 = (-90.0+50.0)*pi/180.0;
+e10 = (-90.0+20.0)*pi/180.0;
+time  = linspace(0,100,20000);
 IC    = [l1*cos(e00); 0; l1*sin(e00); 0; e10; 0];
 [t,y] = ode45('ode',time,IC);
 
@@ -28,7 +28,7 @@ legend('theta','r')
 figure
 O    = [0 0];
 axis(gca,'equal');
-axis([-1.0 1.0 -2.0 1.0]);
+axis([-1.0 1.0 -2.5 0.5]);
 grid on;
 origincircle = viscircles(O,0.001);
 for j = 1:5
@@ -41,7 +41,7 @@ for j = 1:5
         bob2         = line([P2(1) P2(1)],[P2(2) P2(2)], 'Marker','o','MarkerSize',13,'MarkerFaceColor','b');
         ball1        = viscircles(P1, 0.007);
         ball2        = viscircles(P2, 0.007);
-        pause(0.0001);
+        pause(0.00005);
         if i<length(t)
             delete(pendulum1);
             delete(bob1);
